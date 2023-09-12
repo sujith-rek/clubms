a7340e549dafe0b6423de7379fe31587ed47f583
Sujith <ps335@snu.edu.in>
2023-09-13 01:33:16 +0530

    test comp

---
+++ components/Test/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Test.scss';
+
+function Test() {
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test
---
+++ pages/meow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Test from '@/components/Test/Test'
+
+function meow() {
+  return (
+    <Test />
+  )
+}
+
+export default meow
