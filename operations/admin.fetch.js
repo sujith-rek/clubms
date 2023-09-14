2c1ca5b7d5e0b1d4bcb6dfedcd84c5eb5565a916
Sujith <ps335@snu.edu.in>
2023-09-14 20:58:16 +0530

    add login register

---
+++ operations/admin.fetch.js
@@ -0,0 +1,40 @@
+export async function loginAdmin(data) {
+    return await fetch(
+        "/api/login",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data),
+            credentials: "include"
+        }
+    ).then((res) => {
+        return res.json()
+    })
+        .catch((err) => {
+            console.log(err)
+        });
+}
+
+export async function registerAdmin(data) {
+    return await fetch(
+        "/api/register",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data),
+            credentials: "include"
+        }).then((res) => {
+            return res.json()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+
+
+
---
+++ services/users.services.js
@@ -0,0 +1,27 @@
+import db from "@/lib/prisma";
+import { compareSync } from "bcrypt";
+
+export async function registerAdmin(data){
+    return await db.admin.create({
+        data
+    })
+}
+
+export async function loginAdmin(data){
+    const admin = await db.admin.findUnique({
+        where:{
+            email:data.email
+        }
+    })
+    if(!admin){
+        throw new Error("No admin found")
+    }
+
+    console.log(admin,data.password)
+
+    const valid = await compareSync(data.password,admin.password)
+    if(!valid){
+        throw new Error("Invalid password")
+    }
+    return admin
+}
