64327a1dc81e235008eccf1e0c63335904d559c2
Sujith <ps335@snu.edu.in>
2023-09-16 15:01:41 +0530

    admin event update

---
+++ pages/api/admin/updateEvent.js
@@ -0,0 +1,19 @@
+import { eventUpdateAdmin } from "@/services/admin.services";
+
+
+export default async function updateEvent(req, res) {
+
+    const { eventId, adminStatus, adminId } = req.body
+    try {
+        const event = await eventUpdateAdmin(eventId, { adminStatus, adminId })
+        res.json({ message: 'Event updated successfully', event })
+    } catch (e) {
+        res.status(400)
+        res.json({ message: e.message })
+    }
+
+
+}
+
+
+
---
+++ prisma/migrations/20230916090234_/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Eventapproval" ALTER COLUMN "adminId" SET DEFAULT 1,
+ALTER COLUMN "ccId" SET DEFAULT 1;
