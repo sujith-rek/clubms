c864bddb434c8e906d8e3b5e3a5a50ce0f9a350e
Sujith <ps335@snu.edu.in>
2023-09-16 14:23:17 +0530

    event utils

--- services/events.service.js
+++ services/events.service.js
@@ -5,3 +5,33 @@ export async function eventCreate(data){
         data
     })
 }
+
+export async function eventUpdate(id, data){
+    return await db.event.update({
+        where: { id },
+        data
+    })
+}
+
+export async function eventDelete(id){
+    return await db.event.delete({
+        where: { id }
+    })
+}
+
+export async function eventFindUnique(id){
+    return await db.event.findUnique({
+        where: { id }
+    })
+}
+
+export async function eventFindMany(){
+    return await db.event.findMany()
+}
+
+export async function eventFindManyByClubId(clubId){
+    return await db.event.findMany({
+        where: { clubId }
+    })
+}
+
