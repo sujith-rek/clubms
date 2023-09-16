bc8930d6fade2ce6efdd0754d8115aed0f78babe
Sujith <ps335@snu.edu.in>
2023-09-16 14:19:20 +0530

    add create event

--- pages/api/event/create.js
+++ pages/api/event/create.js
@@ -1,4 +1,24 @@
+import { EventSchema } from "@/schemas/event.schema"
+import { eventCreate } from "@/services/events.service"
+
 
 export default async function createEvent(req, res) {
-  res.json({ message: 'Hello Everyone!' })
+  const { name, description, date, venue, clubId } = req.body
+  const data = {
+    name,
+    description,
+    date: new Date(date),
+    venue,
+    clubId
+  }
+  console.log(data)
+  try {
+    
+    EventSchema.parse(data)
+    const event = await eventCreate(data)
+    res.json({ message: 'Event created successfully', event })
+  } catch (e) {
+    res.status(400)
+    res.json({ message: e.message })
+  }
 }
