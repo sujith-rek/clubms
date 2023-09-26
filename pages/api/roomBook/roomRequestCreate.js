3703936924c7d6453334ae24d14b427ad2d3368e
Sujith <ps335@snu.edu.in>
2023-09-27 00:52:14 +0530

    update room book schema

--- pages/api/roomBook/roomRequestCreate.js
+++ pages/api/roomBook/roomRequestCreate.js
@@ -1,4 +1,7 @@
 import { roombookCreate } from "@/services/roombook.services"
+
 export default async function bookRequestCreate(req, res) {
     
+    const { room, date, time, user } = req.body
+
 }
--- pages/api/roomBook/roomRequestUpdate.js
+++ pages/api/roomBook/roomRequestUpdate.js
@@ -1,3 +1,4 @@
+import { roombookUpdate } from "@/services/roombook.services"
 
 export default async function bookRequestUpdate(req, res) {
     res.json({ message: 'Request updated.' })
