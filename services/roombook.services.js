ef7d6baf7de0e675ade4672848c8c4ebd6d9087a
Sujith <ps335@snu.edu.in>
2023-09-26 12:17:48 +0530

    add room services

---
+++ services/roombook.services.js
@@ -0,0 +1,53 @@
+import db from "@/lib/prisma";
+
+
+export async function roombookCreate(data){
+    return await db.roombookapproval.create({
+        data
+    })
+}
+
+
+export async function roombookUpdate(roombookId, data){
+    return await db.roombookapproval.update({
+        where: { id: roombookId },
+        data
+    })
+}
+
+
+export async function roombookDelete(roombookId){
+    return await db.roombookapproval.delete({
+        where: { id: roombookId }
+    })
+}
+
+
+export async function approveRoomBook(roombookId, data){
+    return await db.roombookapproval.update({
+        where: { id: roombookId },
+        data
+    })
+}
+
+
+export async function addRooms(data){
+    return await db.room.create({
+        data
+    })
+}
+
+export async function updateRoom(roomId, data){
+    return await db.room.update({
+        where: { id: roomId },
+        data
+    })
+}
+
+export async function deleteRoom(roomId){
+    return await db.room.delete({
+        where: { id: roomId }
+    })
+}
+
+
