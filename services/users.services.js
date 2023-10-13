a13f894f3f4bf9cf1d2266633cf9b07ac18b34d7
Ary0405 <aryansethia2003@gmail.com>
2023-10-13 10:15:30 +0530

    club auth endpoints added

--- services/users.services.js
+++ services/users.services.js
@@ -1,26 +1,40 @@
 import db from "@/lib/prisma";
 import { compareSync } from "bcrypt";
 
-export async function registerAdmin(data){
+export async function registerAdmin(data) {
     return await db.admin.create({
         data
     })
 }
 
-export async function loginAdmin(data){
+export async function registerClub(data) {
+    return await db.club.create({
+        data
+    })
+}
+
+export async function loginAdmin(data) {
     const admin = await db.admin.findUnique({
-        where:{
-            email:data.email
+        where: {
+            email: data.email
         }
     })
-    if(!admin){
+    if (!admin) {
         throw new Error("No admin found")
     }
 
-    const valid = compareSync(data.password,admin.password)
-    
-    if(!valid){
+    const valid = compareSync(data.password, admin.password)
+
+    if (!valid) {
         throw new Error("Invalid password")
     }
     return admin
+}
+
+export async function loginClub(data) {
+    return db.club.findUnique({
+        where: {
+            email: data.email,
+        }
+    })
 }
